988884db90063371f952a8e75e843e73979f568b
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-26 09:38:15 +0400

    add: global.css caribles couleurs, police, les titres et paragraphes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,17 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="Home">container</div>
+      <h1>Titre h1</h1>
+      <h1 className="BigTitle">Big Titre h1</h1>
+      <h2>Titre h2</h2>
+      <h3>Titre h3</h3>
+      <h4>Titre h4</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In provident
+        odit laudantium, iusto placeat, repellendus dolorem asperiores,
+        perspiciatis sapiente harum veniam. Porro nihil reprehenderit rerum
+        nostrum beatae nemo nulla animi?
+      </p>
     </div>
   );
 }
